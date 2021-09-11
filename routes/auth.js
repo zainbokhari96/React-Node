3852fa3fb20260edb2a9d507437f046304f61bfb
Syed Zain Ali Bokhari <seydzainbokhari@outlook.com>
2021-09-12 03:12:29 +0500

    Add JWT For Login Token

--- routes/auth.js
+++ routes/auth.js
@@ -4,10 +4,14 @@ const router = express.Router();
 // For Express Validation
 const { body, validationResult } = require("express-validator");
 // Import Bycrypt Librar For Password Encryption
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
+//  Import JWT For Login Token
+const jwt = require('jsonwebtoken');
+// Private Key Must Be Save In ENV Files Not Here , Must Read From ENV Files In Production
+const privateKey = 'ZainPrivateKeyForTesting'
 
 
-// Signup Route For User , Doesnot Require Auth
+// Signup Route For User , Doesnot Require Login
 router.post(
   "/signup", // username must be an email
   body("email", "Enter A Valid Email").isEmail(),
@@ -25,6 +29,7 @@ router.post(
       if (user) {
         return res.status(401).json({ error: "Email Is Already Registered !" });
       }
+      //   Add Encryption For Password Hashing
       let salt = await bcrypt.genSaltSync(10);
       let hashedPassword = await bcrypt.hashSync(req.body.password, salt);
       user = await User.create({
@@ -32,7 +37,42 @@ router.post(
         email: req.body.email,
         password: hashedPassword,
       });
-      return res.status(200).json({message : "User Created Successfully !"})
+      return res.status(200).json({ message: "User Created Successfully !" });
+    } catch (err) {
+      console.log(err);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
+
+// Login Route For User , Doesnot Require Login
+router.post(
+  "/login", // username must be an email
+  body("email", "Enter A Valid Email").isEmail(),
+  // password must be at least 5 chars long
+  body("password", "Password Cannot Be Blank").exists(),
+  async (req, res) => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // Try Catch For Async await
+    try {
+      const { email, password } = req.body;
+      let user = await User.findOne({email});
+      if (!user) {
+        return res.status(401).json({ error: "User Not Exists" });
+      }
+      //   Compare Encrpyted Password From DB
+      const comparedPassword = await bcrypt.compare(password, user.password);
+      if (!comparedPassword) {
+        return res
+          .status(401)
+          .json({ message: "Please Login With Correct Credentials" });
+      }
+      const token = await jwt.sign({ id: user.id }, privateKey);
+      return res.status(200).json({token:token});
     } catch (err) {
       console.log(err);
       res.status(500).send("Internal Server Error");
